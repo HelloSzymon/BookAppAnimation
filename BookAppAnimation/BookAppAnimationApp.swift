02f6a98215415d5a8566bfdcb6af86279639d761
Szymon Wnuk <szymon.wnuk@icloud.com>
2022-11-08 11:16:08 +0100

    Initial Commit

---
+++ BookAppAnimation/BookAppAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppAnimationApp.swift
+//  BookAppAnimation
+//
+//  Created by Szymon Wnuk on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookAppAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
